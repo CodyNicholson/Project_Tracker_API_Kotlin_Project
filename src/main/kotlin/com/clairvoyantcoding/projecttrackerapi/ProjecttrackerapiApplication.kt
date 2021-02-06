f7ebf41a28babd348188edb78f33e8c643031e3c
Cody Nicholson <codynichoson96@gmail.com>
2021-02-06 08:01:29 -0600

    init kotlin app

---
+++ src/main/kotlin/com/clairvoyantcoding/projecttrackerapi/ProjecttrackerapiApplication.kt
@@ -0,0 +1,11 @@
+package com.clairvoyantcoding.projecttrackerapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProjecttrackerapiApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProjecttrackerapiApplication>(*args)
+}
---
+++ src/test/kotlin/com/clairvoyantcoding/projecttrackerapi/ProjecttrackerapiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.clairvoyantcoding.projecttrackerapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProjecttrackerapiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
